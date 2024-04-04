933219d98c7e6ca54bb1e78fc701f24c84359a05
akankshanighut <105767837+akankshanighut@users.noreply.github.com>
2024-04-04 15:15:02 +0530

    Update DupliChar.java

--- DupliChar.java
+++ DupliChar.java
@@ -14,15 +14,14 @@ public class DupliChar
 {  
      public static void main(String[] args) 
      {  
-         //Take input from the user
         Scanner sc =new Scanner(System.in);  
         System.out.println("Enter the string is: ");
         String str1= sc.nextLine();
         int count;  
-        //Converts given string into character array  
+          
         char str[] = str1.toCharArray();            
         System.out.println("Duplicate characters in the given string: ");  
-        //Counts each character present in the string  
+        
         for(int i = 0; i <str.length; i++) 
         {  
             count = 1;  
@@ -31,11 +30,11 @@ public class DupliChar
                 if(str[i] == str[j] && str[i] != ' ') 
                 {  
                     count++;  
-                    //Set string[j] to 0 to avoid printing visited character  
+                    
                     str[j] = '0';  
                 }  
             }  
-            //A character is considered as duplicate if count is greater than 1  
+            
             if(count > 1 && str[i] != '0')  
                 System.out.println(str[i]);  
         }  
